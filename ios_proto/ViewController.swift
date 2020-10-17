c526fa4bc841129c65b5207da00d64c713450328
Sheridan Gomes <srei0008@student.monash.edu>
2020-10-17 21:02:26 +1100

    Initial Commit

---
+++ ios_proto/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_proto
+//
+//  Created by Sheridan's Lair on 17/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
