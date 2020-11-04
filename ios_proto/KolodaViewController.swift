20883602be6023fd720744acb62b677369334369
Sheridan Gomes <srei0008@student.monash.edu>
2020-11-05 07:14:37 +1100

    koloda done

---
+++ ios_proto/KolodaViewController.swift
@@ -0,0 +1,64 @@
+//
+//  KolodaViewController.swift
+//  ios_proto
+//
+//  Created by Sheridan's Lair on 05/11/20.
+//
+
+import UIKit
+import Koloda
+
+class KolodaViewController: UIViewController, KolodaViewDelegate, KolodaViewDataSource {
+    
+    
+    @IBOutlet weak var myContentsKolodaView: KolodaView!
+    
+    var imageStock = [UIImage]() // array of images
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchImages()
+        setupForKoloda()
+        reloadImages()
+    }
+    
+    // fetch added images and appended to imageStock
+    func fetchImages() {
+        guard let basketball = UIImage(named: "basketball.png"), let football = UIImage(named: "football.png"), let rugby = UIImage(named: "rugby.png") else { return } // fetch images
+        imageStock.append(basketball)
+        imageStock.append(football)
+        imageStock.append(rugby)
+    }
+    
+    // initialized koloda
+    func setupForKoloda() {
+        
+        myContentsKolodaView.delegate = self
+        myContentsKolodaView.dataSource = self
+    }
+    
+    func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
+        return UIImageView(image: imageStock[index]) // sorting
+    }
+    
+    func kolodaSpeedThatCardShouldDrag(_ koloda: KolodaView) -> DragSpeed {
+        return .fast // fast swiping
+    }
+    
+    func kolodaNumberOfCards(_ koloda: KolodaView) -> Int {
+        return imageStock.count // images count
+    }
+    
+    func reloadImages() {
+        myContentsKolodaView.reloadData() // reload kolodaView after images pull the images
+    }
+    
+    func kolodaDidRunOutOfCards(_ koloda: KolodaView) {
+        fetchImages() // pull images again
+        reloadImages() // reload kolodaView
+    }
+    
+}
+
+
+
