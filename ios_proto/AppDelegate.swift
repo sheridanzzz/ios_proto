55191ff012efa1b45fd443fe11f6cf5d923a81ab
Hamza <hamzajaved70@gmail.com>
2020-11-15 23:30:14 +1100

    added register page

--- ios_proto/AppDelegate.swift
+++ ios_proto/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         UITabBar.appearance().barTintColor = .black
         UITabBar.appearance().tintColor = .red
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
--- ios_proto/RegisterViewController.swift
+++ ios_proto/RegisterViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var fName_textField: UITextField!
+    @IBOutlet weak var lName_textField: UITextField!
+    @IBOutlet weak var gender_pickerView: UIPickerView!
+    @IBOutlet weak var dob_datePicker: UIDatePicker!
+    @IBOutlet weak var state_textField: UITextField!
+    @IBOutlet weak var postcode_textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func signUp_button(_ sender: Any) {
+        
+    }
+    
 }
