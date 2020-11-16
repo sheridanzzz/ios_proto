1ec6577244951a7747faf0da4a9b51fd8dbbdeb4
Hamza <hamzajaved70@gmail.com>
2020-11-16 23:02:29 +1100

    auth

--- ios_proto/RegisterViewController.swift
+++ ios_proto/RegisterViewController.swift
@@ -12,18 +12,31 @@ class RegisterViewController: UIViewController {
 
     @IBOutlet weak var fName_textField: UITextField!
     @IBOutlet weak var lName_textField: UITextField!
+    @IBOutlet weak var email_textField: UITextField!
+    @IBOutlet weak var password_textField: UITextField!
     @IBOutlet weak var gender_pickerView: UIPickerView!
-    @IBOutlet weak var dob_datePicker: UIDatePicker!
+    @IBOutlet weak var date_datePicker: UIDatePicker!
     @IBOutlet weak var state_textField: UITextField!
     @IBOutlet weak var postcode_textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func signUp_button(_ sender: Any) {
-        
+    @IBAction func register_button(_ sender: Any) {
+        if let email = email_textField.text, let password = password_textField.text {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                }
+                else {
+                    
+                }
+            }
+        }
     }
+    
     
 }
