b4040f09e27053a94d218023669a0ba5185d7d3b
Hamza <hamzajaved70@gmail.com>
2020-11-21 22:54:07 +1100

    fixed sign up

--- ios_proto/RegisterViewController.swift
+++ ios_proto/RegisterViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
-class RegisterViewController: UIViewController {
+class RegisterViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var email_textField: UITextField!
     @IBOutlet weak var pass_textField: UITextField!
@@ -19,15 +20,42 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var date_datePicker: UIDatePicker!
     @IBOutlet weak var state_textField: UITextField!
     @IBOutlet weak var postcode_textField: UITextField!
+    
+    var selectedGender: String?
+    var gender_text: String?
+    var pickerData: [String] = [String]()
+    var dateText: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
         let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
+        self.gender_pickerView.dataSource = self
+        self.gender_pickerView.delegate = self
+        pickerData = ["Male", "Female", "Not Specified"]
+    }
+    
+    @IBAction func datePicker_changed(_ sender: Any) {
+        let dateFormatter = DateFormatter()
+
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        dateFormatter.timeStyle = DateFormatter.Style.short
+
+        let strDate = dateFormatter.string(from: date_datePicker.date)
+        dateText = strDate
     }
     
     @IBAction func register_button(_ sender: Any) {
+        let firstName = fname_textField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let lastName = lname_textField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let state = state_textField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let postcode = postcode_textField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd/MM/yyyy"
+        
         if let email = email_textField.text, let password = retype_pass_textField.text {
             Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
                 if let e = error {
@@ -40,6 +68,15 @@ class RegisterViewController: UIViewController {
                 }
                 else {
                     // after login is done, maybe put this in the login web service completion block
+                    
+                    let db = Firestore.firestore()
+                    db.collection("users").addDocument(data: ["firstName":firstName, "lastName":lastName, "gender":self.gender_text, "state":state, "postcode":postcode, "uuid":authResult?.user.uid, "dateOfBirth":self.dateText, "registerationDate":dateFormatter.string(from: date)]) { (error) in
+                        
+                        if error != nil {
+                            //show error message
+                            print(error)
+                        }
+                    }
                     
                     let storyboard = UIStoryboard(name: "Main", bundle: nil)
                     let mainTabBarController = storyboard.instantiateViewController(identifier: "MainTabBarController")
@@ -73,13 +110,37 @@ class RegisterViewController: UIViewController {
             //
             //        displayMessage(title: "Not all fields filled", message: errorMsg)
         }
-        
-        func displayMessage(title: String, message: String) {
-            let alertController = UIAlertController(title: title, message: message,
-                                                    preferredStyle: UIAlertController.Style.alert)
-            alertController.addAction(UIAlertAction(title: "Dismiss", style:
-                                                        UIAlertAction.Style.default,handler: nil))
-            self.present(alertController, animated: true, completion: nil)
-        }
+    }
+    
+    func displayMessage(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message,
+                                                preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style:
+                                                    UIAlertAction.Style.default,handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // Number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return fopr the row and component (column) that's being passed in
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
+    // Capture the picker view selection
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+           // This method is triggered whenever the user makes a change to the picker selection.
+           // The parameter named row and component represents what was selected.
+        selectedGender = pickerData[row]
+        gender_text = selectedGender
     }
 }
+
