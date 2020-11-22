c68c64fb9b4b51b3c39b8eb66e9c0c71ffe5c98b
Sheridan Gomes <srei0008@student.monash.edu>
2020-11-23 06:31:56 +1100

    map

--- ios_proto/MapViewController.swift
+++ ios_proto/MapViewController.swift
@@ -55,16 +55,16 @@ class MapViewController: UIViewController, MKMapViewDelegate, DatabaseListener {
     }
     
     //adds annotations to the map view first
-    private lazy var addAnno: Void = {
-        for event in allEvents {
-            let location = LocationAnnotation(title: event.eventName ?? "", subtitle: event.locationName ?? "", image: event.annotationImg ?? "", lat: event.lat ?? 0.0 , long: event.long ?? 0.0)
-            eventsList.append(location)
-        }
-        
-        for i in eventsList{
-            mapView.addAnnotation(i)
-        }
-    }()
+//    private lazy var addAnno: Void = {
+//        for event in allEvents {
+//            let location = LocationAnnotation(title: event.eventName ?? "", subtitle: event.locationName ?? "", image: event.annotationImg ?? "", lat: event.lat ?? 0.0 , long: event.long ?? 0.0)
+//            eventsList.append(location)
+//        }
+//
+//        for i in eventsList{
+//            mapView.addAnnotation(i)
+//        }
+//    }()
     
     lazy var removeAnno: Void = {
         print(eventsList.count)
@@ -76,7 +76,14 @@ class MapViewController: UIViewController, MKMapViewDelegate, DatabaseListener {
     func onEventListChange(change: DatabaseChange, events: [Events]) {
         allEvents = events
         print (allEvents.count)
-        _ = addAnno
+        for event in allEvents {
+            let location = LocationAnnotation(title: event.eventName ?? "", subtitle: event.sport ?? "", image: event.annotationImg ?? "", lat: event.lat ?? 0.0 , long: event.long ?? 0.0)
+            eventsList.append(location)
+        }
+        
+        for i in eventsList{
+            mapView.addAnnotation(i)
+        }
     }
     
     func onSportListChange(change: DatabaseChange, sports: [Sports]) {
@@ -96,55 +103,142 @@ class MapViewController: UIViewController, MKMapViewDelegate, DatabaseListener {
     }
     
     //too add annotation images
+//    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "AnnotationView")
+//
+//        if annotationView == nil {
+//            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "AnnotationView")
+//        }
+//
+//        annotationView?.image = nil
+//        exName = ""
+//        exDes = ""
+//        lat = 0.0
+//        long = 0.0
+//
+//        let cpa = annotation as! LocationAnnotation
+//        annotationView?.image = UIImage(named: cpa.image ?? "")
+//        annotationView?.canShowCallout = true
+//        annotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+//
+//        return annotationView
+//    }
+    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "AnnotationView")
-        
-        if annotationView == nil {
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "AnnotationView")
+            if annotation.isKind(of: MKUserLocation.self) {  //Handle user location annotation..
+                return nil  //Default is to let the system handle it.
+            }
+
+            if !annotation.isKind(of: ImageAnnotation.self) {  //Handle non-ImageAnnotations..
+                var pinAnnotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "DefaultPinView")
+                if pinAnnotationView == nil {
+                    pinAnnotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "DefaultPinView")
+                    pinAnnotationView?.canShowCallout = true
+                    pinAnnotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+                }
+                return pinAnnotationView
+            }
+
+            //Handle ImageAnnotations..
+            var view: ImageAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: "imageAnnotation") as? ImageAnnotationView
+            if view == nil {
+                view = ImageAnnotationView(annotation: annotation, reuseIdentifier: "imageAnnotation")
+            }
+
+            let annotation = annotation as! ImageAnnotation
+            view?.image = annotation.image
+            view?.annotation = annotation
+
+            return view
         }
-        
-        annotationView?.image = nil
-        exName = ""
-        exDes = ""
-        lat = 0.0
-        long = 0.0
-        
-        let cpa = annotation as! LocationAnnotation
-        annotationView?.image = UIImage(named: cpa.image ?? "")
-        annotationView?.canShowCallout = true
-        annotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
-        
-        return annotationView
-    }
     
     var loco : MKPointAnnotation!
     
     //send data to exhibition details view
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-//    {
-//        if segue.destination is ExhibitionDetailsViewController
-//        {
-//            let ed = segue.destination as? ExhibitionDetailsViewController
-//            ed?.name = exName
-//            ed?.des = exDes
-//            ed?.lat = lat
-//            ed?.long = long
-//            ed?.icon = imageIcon
-//            ed?.pin = loco
-//        }
-//    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.destination is EventsDetailsViewController
+        {
+            let ed = segue.destination as? EventsDetailsViewController
+            ed?.name = exName
+            ed?.sportType = exDes
+            ed?.lat = lat
+            ed?.long = long
+            ed?.icon = imageIcon
+            ed?.pin = loco
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "dd-MM-yyyy HH:mm:ss"
+            
+            for event in allEvents{
+                if exName == event.eventName {
+                    ed?.dateTime = dateFormatter.string(from: event.eventDateTime!)
+                    ed?.noOfPlayers = String(event.numberOfPlayers!)
+                    ed?.minNoOfPlayers = String(event.minNumPlayers!)
+                    ed?.location = event.locationName!
+                    ed?.icon = event.annotationImg!
+                }
+            }
+        }
+    }
     
     //annotation call out to move to the next view
-//    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
-//        if control == view.rightCalloutAccessoryView {
-//            loco = view.annotation as? MKPointAnnotation
-//            exName = view.annotation!.title!!
-//            exDes = view.annotation!.subtitle!!
-//            lat = view.annotation!.coordinate.latitude
-//            long = view.annotation!.coordinate.longitude
-//
-//            performSegue(withIdentifier: "exhibitionDetailsSegue", sender: self)
-//        }
-//    }
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            loco = view.annotation as? MKPointAnnotation
+            exName = view.annotation!.title!!
+            exDes = view.annotation!.subtitle!!
+            lat = view.annotation!.coordinate.latitude
+            long = view.annotation!.coordinate.longitude
+
+            performSegue(withIdentifier: "mapEventsSegue", sender: self)
+        }
+    }
+    
+    class ImageAnnotation : NSObject, MKAnnotation {
+        var coordinate: CLLocationCoordinate2D
+        var title: String?
+        var subtitle: String?
+        var image: UIImage?
+        var colour: UIColor?
+
+        override init() {
+            self.coordinate = CLLocationCoordinate2D()
+            self.title = nil
+            self.subtitle = nil
+            self.image = nil
+            self.colour = UIColor.white
+        }
+    }
+
+    class ImageAnnotationView: MKAnnotationView {
+        private var imageView: UIImageView!
+
+        override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+            super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+
+            self.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+            self.imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+            self.addSubview(self.imageView)
+
+            self.imageView.layer.cornerRadius = 5.0
+            self.imageView.layer.masksToBounds = true
+        }
+
+        override var image: UIImage? {
+            get {
+                return self.imageView.image
+            }
+
+            set {
+                self.imageView.image = newValue
+            }
+        }
+
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("init(coder:) has not been implemented")
+        }
+    }
+
 
 }
