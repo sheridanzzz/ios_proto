87828b3a2240d646071b7f08b819c14a9970bd22
Hamza <hamzajaved70@gmail.com>
2020-11-23 19:20:50 +1100

    temperature

--- ios_proto/EventDayViewController.swift
+++ ios_proto/EventDayViewController.swift
@@ -10,10 +10,42 @@ import WebKit
 
 class EventDayViewController: UIViewController {
 
+    @IBOutlet weak var currentTemp_label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let url = URL(string: "http://hamza123.sytes.net:5000/")
+        guard let requestUrl = url else { fatalError() }
+        // Create URL Request
+        var request = URLRequest(url: requestUrl)
+        // Specify HTTP Method to use
+        request.httpMethod = "GET"
+        // Send HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check if Error took place
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Read HTTP Response Status code
+            if let response = response as? HTTPURLResponse {
+                print("Response HTTP Status code: \(response.statusCode)")
+            }
+            
+            // Convert HTTP Response Data to a simple String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+                self.currentTemp_label.text = dataString
+            }
+            
+        }
+        task.resume()
+    }
+    
+    @IBAction func viewStream_button(_ sender: Any) {
         let webView = WKWebView(frame: view.frame)
         view.addSubview(webView)
         
@@ -22,7 +54,6 @@ class EventDayViewController: UIViewController {
         webView.load(request)
     }
     
-
     /*
     // MARK: - Navigation
 
